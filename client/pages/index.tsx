230da06fc5703fe67d8e78cfda27ecf620469f61
Deshario <sunil@iapp.co.th>
2021-08-04 23:19:46 +0700

    PieChart Added

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,14 +1,19 @@
-import { Row, Card, List, Avatar } from 'antd';
+import { Row, Card, List, Avatar, Col } from 'antd';
 import { useEffect, useState } from 'react';
 import { useRecoilValue } from 'recoil';
 import { keySecret } from '../recoils/atoms/keySecret'
 import { useQuery } from '@apollo/client'
+import PieChart from "highcharts-react-official";
+import Highcharts from "highcharts/highstock";
 import { QUERY_BALANCE } from '../documents'
 
 const Home = () => {
 
   const credentials = useRecoilValue(keySecret);
-  const [balances, setBalances] = useState([])
+  const [balances, setBalances] = useState({
+    listData: [],
+    pieData: [],
+  })
 
   const { data, loading, error } = useQuery(QUERY_BALANCE, {
     variables:{
@@ -41,16 +46,31 @@ const Home = () => {
         }
       });
 
-      setBalances(totalBalances)
+      const chartData = mData.balances.map((eachItem:any) => {
+        const available = Number(eachItem.available);
+        const value = Number(eachItem.value);
+        const totalValue = Number(available*value).toFixed(2)
+        return {
+          name: eachItem.symbol,
+          y: Number(totalValue)
+        }
+      });
+
+      setBalances({
+        listData: totalBalances,
+        pieData: chartData,
+      })
+      
     }
     if (error) console.log(`Err: ${error}`)
   }, [loading, error, data])
-  
+
   return (
     <Row gutter={16}>
-       <Card hoverable style={{ width: 400 }}>
-        <List
-            dataSource={balances}
+      <Col>
+        <Card hoverable style={{ width: 400 }}>
+          <List
+            dataSource={balances.listData}
             renderItem={(item:any) => (
               <List.Item key={`${item.symbol}_${item.available}`}>
                 <List.Item.Meta
@@ -66,9 +86,28 @@ const Home = () => {
                 </div>
               </List.Item>
             )}>
-        </List>
+          </List>
       </Card>
-   
+      </Col>
+      <Col>
+        <PieChart
+          highcharts={Highcharts}
+          options={{
+            chart: {
+              type: "pie"
+            },
+            title: {
+              text: 'Total Holdings'
+            },
+            credits: {
+              enabled: false
+            },
+            series: [{
+              data: balances.pieData
+            }],
+          }}
+        />
+      </Col>
     </Row>
   )
 }
